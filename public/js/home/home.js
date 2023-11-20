fc749b3cc062de70ac3a07ef88d10fa5f3c0b4c0
Julien Dulac <contact@juliendulac.fr>
2023-11-20 18:56:13 +0100

    initial commit example controller + default route

---
+++ public/js/home/home.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function(){
+    let btn = document.getElementById('btn');
+
+    btn.addEventListener('click', function(e){
+        e.preventDefault()
+        alert('Ajout JS')
+    })
+})
