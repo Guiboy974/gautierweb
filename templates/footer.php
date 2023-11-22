de5b87ab607b5936ea2133c6f43d09a9e03a6b43
Guillaume Boyer <guiboy97.410@gmail.com>
2023-11-22 17:04:02 +0100

    home

---
+++ templates/footer.php
@@ -0,0 +1,4 @@
+<div class="card-footer text-center text-body-secondary my-5">
+©copyright, tous droits réservés<br>
+<a href="https://fr.freepik.com/vecteurs-libre/fond-encre-alcool-peint-main-paillettes-or_49256435.htm#query=fond%20artistique&position=1&from_view=search&track=ais&uuid=4b41d3b4-2c8a-4306-8001-55f45c26040a">fond de kjpargeter</a>
+</div> 
---
+++ templates/header.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg my-4>
+      <div class="container-fluid">
+        <a class="navbar-brand mb-1 fw-bold" style="font-size: 3.5rem" href="home/home.html.twig"><strong>Gautier | Artiste-Peintre</a></strong>
+        <button class="navbar-toggler fs-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-lg-0 fw-semibold" style="font-size: 2.5rem">
+            <li class="nav-item">
+              <a class="nav-link" href="#">L'artiste</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Galerie</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
