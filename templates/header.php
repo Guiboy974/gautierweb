41dc24162754c4f26c26afe9f65cdf551da9b7a9
Guillaume Boyer <guiboy97.410@gmail.com>
2023-11-23 09:47:16 +0100

    ajusted

--- templates/header.php
+++ templates/header.php
@@ -1,11 +1,11 @@
-<nav class="navbar navbar-expand-lg my-4>
+<nav class="navbar nav-underline navbar-expand-lg my-4>
       <div class="container-fluid">
         <a class="navbar-brand mb-1 fw-bold" style="font-size: 3.5rem" href="home/home.html.twig"><strong>Gautier | Artiste-Peintre</a></strong>
         <button class="navbar-toggler fs-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-lg-0 fw-semibold" style="font-size: 2.5rem">
+          <ul class="navbar-nav ms-auto mb-lg-0 fw-semibold" style="font-size: 2rem">
             <li class="nav-item">
               <a class="nav-link" href="#">L'artiste</a>
             </li>
