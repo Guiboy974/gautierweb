69cac2134a8d41bf7cf0f92f63a20ca8dc3b1b83
Guillaume Boyer <guiboy97.410@gmail.com>
2024-01-10 15:15:58 +0100

    maj form

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,7 @@
               <a class="nav-link" href="#">Galerie</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="contact.html.twig">
                 Contact
               </a>
             </li>
