5e3597da90d7378f77ed096974b61d19162b20c5
Guillaume Boyer <guiboy97.410@gmail.com>
2024-01-15 15:59:48 +0100

    js

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -5,6 +5,9 @@ const pieces = await reponse.json();
 const peinture = animaux[0];
 const imagePeinture = document.createElement("img");
 imagePeinture.src = peinture.animaux;
+const nomPeinture = document.createElement("h2");
+nomPeinture.innerText = peinture.nom;
 
 const divGalerie1 = document.querySelector(".galerie1");
 divGalerie1.appendChild(imagePeinture);
+divGalerie1.appendChild(nomPeinture);
