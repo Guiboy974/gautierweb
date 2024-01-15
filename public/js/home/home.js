3fc7e98d22bcc60ce500c40d6622fa7f5b0e050e
Guillaume Boyer <guiboy97.410@gmail.com>
2024-01-15 14:49:54 +0100

    début galerie

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -1,8 +1,10 @@
-window.addEventListener('load', function(){
-    let btn = document.getElementById('btn');
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("../src/galerie.json");
+const pieces = await reponse.json();
 
-    btn.addEventListener('click', function(e){
-        e.preventDefault()
-        alert('Ajout JS')
-    })
-})
+const peinture = animaux[0];
+const imagePeinture = document.createElement("img");
+imagePeinture.src = peinture.animaux;
+
+const divGalerie1 = document.querySelector(".galerie1");
+divGalerie1.appendChild(imagePeinture);
