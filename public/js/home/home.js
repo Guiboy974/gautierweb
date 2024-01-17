d29e50c0de84e85a1af9d55e0546e0cfec6a146f
Guillaume Boyer <guiboy97.410@gmail.com>
2024-01-17 10:12:46 +0100

    Modif

--- public/js/home/home.js
+++ public/js/home/home.js
@@ -1,13 +1,17 @@
 // Récupération des pièces depuis le fichier JSON
-const reponse = await fetch("../src/galerie.json");
-const pieces = await reponse.json();
+// const reponse = await fetch("../src/galerie.json");
+// const animaux = await reponse.json();
 
-const peinture = animaux[0];
-const imagePeinture = document.createElement("img");
-imagePeinture.src = peinture.animaux;
-const nomPeinture = document.createElement("h2");
-nomPeinture.innerText = peinture.nom;
+// for (let i = 0; i < animaux.length; i++){
 
-const divGalerie1 = document.querySelector(".galerie1");
-divGalerie1.appendChild(imagePeinture);
-divGalerie1.appendChild(nomPeinture);
+// const peinture = animaux[i];
+// const imagePeinture = document.createElement("img");
+// imagePeinture.src = peinture.images;
+// const nomPeinture = document.createElement("h2");
+// nomPeinture.innerText = peinture.nom;
+
+// const divGalerie1 = document.querySelector(".galerie1");
+// divGalerie1.appendChild(imagePeinture);
+// divGalerie1.appendChild(nomPeinture);
+// }
+
