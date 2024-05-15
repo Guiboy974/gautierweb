65eb9787c419fd673e5ee4e34241365b46b7b0ee
Julien Dulac <contact@juliendulac.fr>
2024-05-15 18:30:40 +0200

    add admin

---
+++ src/Controller/Admin/AdminController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Categorie;
+use App\Entity\Image;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render("admin/dashboard.html.twig");
+
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Gautierweb');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToRoute('Retourner au site', 'fa fa-arrow-rotate-left', 'home');
+
+        yield MenuItem::linkToDashboard('Tableau de bord', 'fa fa-home');
+        yield MenuItem::SubMenu('Catégories', 'fa fa-list')->setSubItems([
+            MenuItem::linkToCrud('Voir les catégories', 'fa fa-list', Categorie::class),
+            MenuItem::linkToCrud('Ajouter catégorie', 'fa fa-plus', Categorie::class)->setAction(Crud::PAGE_NEW)
+        ]);
+        yield MenuItem::SubMenu('Medias', 'fa fa-photo-video')->setSubItems([
+            MenuItem::linkToCrud('Médiathèque', 'fa fa-list', Image::class),
+            MenuItem::linkToCrud('Ajouter une image', 'fa fa-plus', Image::class)->setAction(Crud::PAGE_NEW)
+        ]);
+    }
+}
---
+++ src/Entity/Image.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ImageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ImageRepository::class)]
+class Image
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $alt_text = null;
+
+    #[ORM\ManyToOne(inversedBy: 'images')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Categorie $categorie = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $filename = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAltText(): ?string
+    {
+        return $this->alt_text;
+    }
+
+    public function setAltText(?string $alt_text): static
+    {
+        $this->alt_text = $alt_text;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?Categorie
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?Categorie $categorie): static
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    public function setFilename(string $filename): static
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+       public function __toString(): string
+    {
+       return $this->name;
+    }
+}
